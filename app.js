d0ebbe1d515b16be8b66e6479ab6c07b6ed87b1c
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-01-03 14:25:21 +0200

    add the validate feature

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ UI.prototype.addBookToList = function (book) {
     list.appendChild(row);
 }
 
+// Show alert
+UI.prototype.showAlert = function (message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+    // Timeout
+    setTimeout(function () {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
 // Clear fields
 UI.prototype.clearFields = function () {
     document.getElementById('title').value = '';
@@ -40,10 +60,18 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     const book = new Book(title, author, isbn);
     // Instantiate UI
     const ui = new UI();
-    // Add book to list
-    ui.addBookToList(book);
-    // Clear fields
-    ui.clearFields();
+    // Validate
+    if (title === '' || author === '' || isbn === '') {
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+        // Show success
+        ui.showAlert('Book Added!', 'success');
+        // Clear fields
+        ui.clearFields();
+    }
+
 
     e.preventDefault();
 })
