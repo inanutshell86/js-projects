40aa6827a428b424aa7bd723530e8a4c2b738ca5
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-01-04 13:22:55 +0200

    replace some code with ES6(arrow functions) syntax

--- app.js
+++ app.js
@@ -2,26 +2,20 @@ document.getElementById('btn1').addEventListener('click', getText);
 document.getElementById('btn2').addEventListener('click', getJSON);
 document.getElementById('btn3').addEventListener('click', getExternal);
 
-// Get local text file
+// Get local text file data
 function getText() {
     fetch('test.txt')
-        .then(function (res) {
-           return res.text();
-        })
-        .then(function (data) {
+        .then(res => res.text())
+        .then(data => {
             document.getElementById('output').innerHTML = data;
         })
-        .catch(function (err) {
-            console.log(err);
-        })
+        .catch(err => console.log(err));
 }
 
 // Get local json data
 function getJSON() {
     fetch('posts.json')
-        .then(function (res) {
-           return res.json();
-        })
+        .then(res => res.json())
         .then(function (data) {
             let output = '';
             data.forEach(function (post) {
@@ -29,17 +23,13 @@ function getJSON() {
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(function (err) {
-            console.log(err);
-        })
+        .catch(err => console.log(err));
 }
 
 // Get data from external API
 function getExternal() {
     fetch('https://api.github.com/users')
-        .then(function (res) {
-           return res.json();
-        })
+        .then(res => res.json())
         .then(function (data) {
             let output = '';
             data.forEach(function (user) {
@@ -47,7 +37,5 @@ function getExternal() {
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(function (err) {
-            console.log(err);
-        })
+        .catch(err => console.log(err));
 }
