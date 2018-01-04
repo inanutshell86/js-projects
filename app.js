781d6f0e1e60e706246e4140c6f7f0ce9614e6ca
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-01-04 12:05:03 +0200

    add POST PUT DELETE requests features

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 const http = new easyHTTP;
 
 // GET posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+/* http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
     if (err) {
         console.log(err);
     } else {
         console.log(posts);
     }
-})
+}); */
+
+// GET single post
+/* http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, posts) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(posts);
+    }
+}); */
+
+// Create data
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+};
+
+// Create post
+/* http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, post) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+}); */
+
+// Update post
+/* http.put('https://jsonplaceholder.typicode.com/posts/5', data, function (err, post) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+}); */
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(response);
+    }
+});
