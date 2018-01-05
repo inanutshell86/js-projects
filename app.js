fbceb6d25beaa1cd07630c461bbc1b08c391ee78
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-01-05 14:33:26 +0200

    add the save location to local storage feature

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+// Init Storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init weather object
-const weather = new Weather('Chernihiv', '74');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 // Inin UI
 const ui = new UI();
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Kyiv', '30');
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    weather.changeLocation(city, state);
+    storage.setLocationData(city, state);
+
+    // Get and display weather
+    getWeather();
+    $('#locModal').modal('hide');
+});
 
 function getWeather() {
     weather.getWeather()
